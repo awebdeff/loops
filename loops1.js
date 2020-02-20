d511a07e60e5105aae9fc0c1c5c3ce691749948e
al-shr <awebdeff@gmail.com>
2020-02-20 16:59:18 +0300

    add nested loop

--- loops1.js
+++ loops1.js
@@ -1,9 +1,22 @@
-let out = document.querySelector('.out')
+// first
+
+let out = document.querySelector('.out1')
 
 for (let i = 0; i < 5; i++) {
   // console.log('*')
   for (let j = 0; j < 10; j++) {
-    out.innerHTML += '*'
+    out.innerHTML += j
   }
-  out.innerHTML += '#'
+  out.innerHTML += '#<br>'
+}
+
+//second
+
+let out2 = document.querySelector('.out2')
+
+for (let b = 2; b < 6; b++) {
+  for (let k = 1; k < 10; k++) {
+    out2.innerHTML += `${b} * ${k} = ${k * b}<br>`
+  }
+  out2.innerHTML += '<hr>'
 }
