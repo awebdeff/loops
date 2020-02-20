01b4ecd97b45ed63cdcecc4781ffbca6dd959677
al-shr <awebdeff@gmail.com>
2020-02-20 17:50:17 +0300

    add while

--- script.js
+++ script.js
@@ -3,11 +3,15 @@
 let out3 = document.querySelector('.out3')
 let tmp = ''
 
+
+
 // отрицательное приращение
 
 // for (let i = 25; i > 0; i= i - 3) {
 //   console.log('i', i)
 // }
+
+
 
 // break & continue
 
@@ -16,6 +20,8 @@ for (let i = 0; i < 10; i++) {
     if (i == 7) break
     console.log('i', i)
   }
+
+
 
 // альтернативные записи
 
@@ -30,6 +36,17 @@ let i = 1
 for ( ; i < 10; ) {
   i++
   if (i == 5) continue
-  if (i == 8) break
-  console.log('i', i)
+  if (i == 8) break  
+  console.log('i', i)  
+}
+
+
+
+// while
+let h = 0;
+while (h < 8) {
+  h++
+  console.log('h', h)
+
+  
 }
